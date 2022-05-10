dda531fb10459b64726f4ce7e834de5b19767844
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-11 05:13:02 +0600

    info card added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Banner } from './Banner'
+import { Info } from './Info'
 
 
 export const Home = () => {
   return (
     <div>
       <Banner/>
+      <Info/>
     </div>
   )
 }
